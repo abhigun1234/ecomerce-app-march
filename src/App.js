d5c8b5b93737b33d90395066f50600965f8fa185
unknown <abhi.ghosh27feb@gmail.com>
2024-04-13 17:56:46 +0530

    add

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ import ProdouctList from './components/ProdouctList';
 import Header from './components/Header'
 import AddNewProduct from './components/AddNewProduct';
 import NoPage from './components/NoPage';
-import AboutUs from './components/AboutUs';
+ import AboutUs from './components/AboutUs';
 
 import Login from './components/Login';
 import Register from './components/Register';
 import React from 'react';
 //import AboutUs from './components/AboutUs';
-//import AboutUs from './components/AboutUs';
+// import AboutUs from './components/AboutUs';
 import ProductTable from './components/ProductTable';
 import ProductDataTable from './components/ProductDataTable'
 import Customer from './components/Customer'
-const LazyAbout=React.lazy(()=>import ('./components/AboutUs'))
+ //const LazyAbout=React.lazy(()=>import ('./components/AboutUs'))
 
 function App() {
   return (
@@ -51,12 +51,12 @@ function App() {
               
               
               }></Route> */}
-              {/* <Route path='/about' element={<AboutUs></AboutUs>}></Route> */}
-              <Route path='/' element={<React.Suspense  fallback='Loding'>
+              <Route path='/about' element={<AboutUs></AboutUs>}></Route>
+              {/* <Route path='/about' element={<React.Suspense  fallback='Loding'>
                 <LazyAbout></LazyAbout>
               </React.Suspense>}>
 
-              </Route>
+              </Route>  */}
               <Route path='/list' element={<ProdouctList/>}></Route>
               <Route path='*' element={<NoPage/>}></Route>
             </Routes>
